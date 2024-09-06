d395713262268b5f9e1c37fa4884132c55c43f83
Chetan Budathoki <chetanbudathoki@outlook.com>
2024-09-06 16:53:50 +0000

    "setup"

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -13,7 +13,7 @@ then
 fi
 
 # Set shorter terminal prompt to just display the directory name
-echo "export PS1='\w: '" >> ~/.bashrc
+echo "export PS1='\W: '" >> ~/.bashrc
 
 # Source the updated .bashrc to apply the changes immediately
 source ~/.bashrc
